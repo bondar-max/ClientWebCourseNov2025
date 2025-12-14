3701e153497659ae45ee954f17c1b0ad7af9711d
bondar-max <maksimusprime@gmail.com>
2025-12-14 23:22:17 +0700

    feat(html): добавление шаблона HTML и скрипта для работы с массивами
    Добавлен новый файл index.html с базовой структурой HTML-документа.
    Включен скрипт script.js, который содержит примеры работы с массивами в JavaScript.
    Эти изменения обеспечивают начальную функциональность для работы с массивами.

---
+++ JS Arrays/script.js
@@ -0,0 +1,33 @@
+/*const x = 3;
+const array = [1, 2, 3, 4, 5];
+console.log(array);
+array.push(6); // добавление в конец массива
+array.push(7, 8); // можно вставлять сколько угодно за раз
+console.log(array);
+array.pop();
+console.log(array);*/
+
+/*const array = [1, 2, 3, 4, 5];
+array.unshift(0); // добавление в начало массива
+console.log(array);
+array.unshift(-2, -1); // можно вставлять много элем-в за раз
+console.log(array);
+array.shift();
+console.log(array);*/
+
+/*const array = [1, 2, 3, 4, 5];
+array.reverse();
+console.log(array.join(", "));*/
+
+/*const array1 = [1, 2, 3];
+const array2 = [4, 5];
+const array3 = array1.concat(array2);
+console.log(array3);*/
+
+const array = [1, 2, 3, 4, 5];
+const array1 = array.slice(1, 4);
+// [2, 3] – подмассив от начального индекса до конечного
+const array2 = array.slice(3);
+// [4, 5] – подмассив от индекса до конца массива
+console.log(array1);
+console.log(array2  );
