f57ce6f826477e88c6ae7388e8dd1bc60833ede2
bondar-max <maksimusprime@gmail.com>
2026-01-13 15:42:53 +0700

    feat(script): добавление функциональности конвертации температуры\n\nДобавлена возможность конвертации температуры из шкалы Цельсия в шкалы\nКельвина и Фаренгейта. Также добавлена обработка ввода некорректных\nзначений с выводом соответствующего сообщения пользователю.

--- DOMTemperatureConverter/script.js
+++ DOMTemperatureConverter/script.js
@@ -1,4 +1,25 @@
-const button = document.querySelector(".my-button");
-button.addEventListener("click", function(e) {
-    alert('Кнопка нажата!');
+document.addEventListener("DOMContentLoaded", function () {
+    const celsiusInputElement = document.getElementById('celsius-scale');
+
+    const kelvinOutputElement = document.getElementById('kelvin-scale');
+    const fahrenheitOutputElement = document.getElementById('fahrenheit-scale');
+
+    kelvinOutputElement.readOnly = true;
+    fahrenheitOutputElement.readOnly = true;
+
+    const convertBtn = document.querySelector('.convert-button');
+    convertBtn.addEventListener("click", function (e) {
+        const celsiusValue = parseFloat(celsiusInputElement.value);
+
+        if (isNaN(celsiusValue)) {
+            alert('Пожалуйста, введите корректное число!');
+            return;
+        }
+
+        const kelvinValue = celsiusValue + 273.15;
+        const fahrenheitValue = (celsiusValue * 9/5) + 32;
+
+        kelvinOutputElement.value = kelvinValue;
+        fahrenheitOutputElement.value = fahrenheitValue;
+    });
 });
