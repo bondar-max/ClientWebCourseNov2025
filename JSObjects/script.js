dfe561d07848b1c214445ba13acde478c4d55b49
bondar-max <maksimusprime@gmail.com>
2026-01-21 11:35:15 +0700

    style(script.js): исправление форматирования объектов в массиве городов

--- JSObjects/script.js
+++ JSObjects/script.js
@@ -4,46 +4,46 @@ const countries = [
     {
         name: "Россия",
         cities: [
-            { name: "Москва", population: 12506468 },
-            { name: "Санкт-Петербург", population: 5351935 },
-            { name: "Новосибирск", population: 1620162 },
-            { name: "Екатеринбург", population: 1493749 }
+            {name: "Москва", population: 12506468},
+            {name: "Санкт-Петербург", population: 5351935},
+            {name: "Новосибирск", population: 1620162},
+            {name: "Екатеринбург", population: 1493749}
         ]
     },
     {
         name: "США",
         cities: [
-            { name: "Нью-Йорк", population: 8336817 },
-            { name: "Лос-Анджелес", population: 3979576 },
-            { name: "Чикаго", population: 2693976 },
-            { name: "Хьюстон", population: 2320268 }
+            {name: "Нью-Йорк", population: 8336817},
+            {name: "Лос-Анджелес", population: 3979576},
+            {name: "Чикаго", population: 2693976},
+            {name: "Хьюстон", population: 2320268}
         ]
     },
     {
         name: "Германия",
         cities: [
-            { name: "Берлин", population: 3769495 },
-            { name: "Гамбург", population: 1847253 },
-            { name: "Мюнхен", population: 1471508 },
-            { name: "Кёльн", population: 1087863 },
-            { name: "Франкфурт", population: 753056 }
+            {name: "Берлин", population: 3769495},
+            {name: "Гамбург", population: 1847253},
+            {name: "Мюнхен", population: 1471508},
+            {name: "Кёльн", population: 1087863},
+            {name: "Франкфурт", population: 753056}
         ]
     },
     {
         name: "Япония",
         cities: [
-            { name: "Токио", population: 13929286 },
-            { name: "Иокогама", population: 3726167 },
-            { name: "Осака", population: 2691185 }
+            {name: "Токио", population: 13929286},
+            {name: "Иокогама", population: 3726167},
+            {name: "Осака", population: 2691185}
         ]
     },
     {
         name: "Франция",
         cities: [
-            { name: "Париж", population: 2148271 },
-            { name: "Марсель", population: 870018 },
-            { name: "Лион", population: 515695 },
-            { name: "Тулуза", population: 479553 }
+            {name: "Париж", population: 2148271},
+            {name: "Марсель", population: 870018},
+            {name: "Лион", population: 515695},
+            {name: "Тулуза", population: 479553}
         ]
     }
 ];
@@ -76,7 +76,7 @@ function calculateCountryPopulation(country) {
         .reduce((sum, city) => sum + city.population, 0);
 }
 
-(function() {
+(function () {
     console.log("Страны с максимальным количеством городов:");
     console.log(getCountriesWithMaxCityCount(countries));
 
