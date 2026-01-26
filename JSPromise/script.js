daf0daf1194d46fcb9bb16b7f06c167100795f6a
bondar-max <maksimusprime@gmail.com>
2026-01-26 16:26:36 +0700

    refactor(script): улучшение структуры функций загрузки данных

--- JSPromise/script.js
+++ JSPromise/script.js
@@ -1,52 +1,52 @@
 "use strict";
 
 (async function () {
+    function loadProductsListUsingPromise() {
+        console.log("Загрузка #1 началась...");
 
-    loadProductsListUsingPromise();
+        fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json")
+            .then(response => {
 
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+                if (!response.ok) {
+                    return new Error(`HTTP ошибка: ${response.status}`);
+                }
 
-    loadProductsListUsingAsyncAwait()
-        .then(data => console.log("Загруженные данные: ", data))
-        .finally(() => {
-            console.log("Загрузка завершена")
-        });
-})();
-
-function loadProductsListUsingPromise() {
-    console.log("Загрузка началась...");
-
-    fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json")
-        .then(response => {
-
-            if (!response.ok) {
-                return new Error("HTTP ошибка: ${response.status}");
-            }
-
-            return response.json()
-        })
-        .then(data => {
-            console.log("Загруженные данные: ", data)
-        })
-        .catch(error => console.log(error.message))
-        .finally(() => {
-            console.log("Загрузка завершена")
-        });
-}
+                return response.json();
+            })
+            .then(data => {
+                console.log("Загруженные данные: ", data);
+            })
+            .catch(error => console.log(error.message))
+            .finally(() => {
+                console.log("Загрузка завершена");
+            });
+    }
 
 // Объявляем асинхронную функцию для загрузки данных
-async function loadProductsListUsingAsyncAwait() {
-    console.log("Загрузка началась...");
+    async function loadProductsListUsingAsyncAwait() {
+        console.log("Загрузка #2 началась...");
 
-    try {
-        const response = await fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+        try {
+            const response = await fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
 
-        if (!response.ok) {
-            return new Error(`HTTP ошибка! Статус: ${response.status}`);
+            if (!response.ok) {
+                return new Error(`HTTP ошибка! Статус: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log("Загруженные данные: ", data);
+            return data;
+        } catch (error) {
+            console.error("Произошла ошибка:", error.message);
+            throw error;
+        } finally {
+            console.log("Загрузка завершена");
         }
-
-        return await response.json();
-    } catch (error) {
-        console.error("Произошла ошибка:", error.message);
     }
-}
+
+    await loadProductsListUsingPromise();
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    await loadProductsListUsingAsyncAwait();
+})();
