aefb6cdbf8cfd9437a72d3d2dff36a5da3e23f0e
bondar-max <maksimusprime@gmail.com>
2026-02-28 19:53:21 +0700

    feat(phoneBookService): добавление сервиса для работы с контактами
    Добавлен класс PhoneBookService, который предоставляет методы для
    загрузки, получения, удаления, добавления и обновления контактов через
    API. Используется библиотека axios для выполнения HTTP-запросов.
    Теперь можно управлять контактами в телефонной книге

---
+++ PhoneBook/frontend/js/phoneBookService.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export default class PhoneBookService {
+    constructor() {
+        this.baseUrl = "/api/contacts"
+    }
+
+    loadContacts(term) {
+        return axios.get(this.baseUrl, {params: {term}})
+            .then(response => response.data);
+    }
+
+    //Получение контакта по ID
+    getContact(id) {
+        return axios.get(`${this.baseUrl}/${id}`)
+            .then(response => response.data);
+    }
+
+    deleteContact(id) {
+        return axios.delete(`${this.baseUrl}/${id}`)
+            .then(response => response.data);
+    }
+
+    addContact(contact) {
+        return axios.post(this.baseUrl, contact)
+            .then(response => response.data);
+
+    }
+
+    //Обновление контакта
+    updateContact(id, contact) {
+        return axios.put(`${this.baseUrl}/${id}`, contact)
+            .then(response => response.data);
+    }
+};
