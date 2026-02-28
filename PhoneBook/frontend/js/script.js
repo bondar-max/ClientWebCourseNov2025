a2f74dcc78b78b7025e299bd9d2cfec3c3ae06c7
bondar-max <maksimusprime@gmail.com>
2026-02-28 19:53:37 +0700

    refactor(js): перемещение и упрощение кода приложения телефонной книги
    Перемещен файл script.js из директории public в frontend.
    Обновлен код для использования нового компонента PhoneBook.vue.
    Упрощены методы и улучшена структура приложения.

---
+++ PhoneBook/frontend/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "../css/style.css";
+
+import {createApp} from "vue";
+import PhoneBook from "./PhoneBook.vue";
+
+createApp(PhoneBook).mount("#app")
